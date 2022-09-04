ac135dd4bbe13f254725f517deb6c4c5500ce80f
Nick Cao <nickcao@nichi.co>
2022-09-04 17:30:05 +0800

    init project skeleton

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;{
-    devShells.x86_64-linux.default = mkShell { };
+    devShells.x86_64-linux.default = mkShell {
+      nativeBuildInputs = [ meson ninja gperf ];
+    };
   };
 }
