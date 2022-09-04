1de79986aae4cd59b894a1629f187c1a0ff93420
Nick Cao <nickcao@nichi.co>
2022-09-04 20:42:47 +0800

    remove vendored copy of gperf

--- flake.nix
+++ flake.nix
@@ -1,9 +1,17 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;{
-    devShells.x86_64-linux.default = mkShell {
-      nativeBuildInputs = [ meson ninja pkg-config clang-tools ];
+    packages.x86_64-linux.default = stdenv.mkDerivation {
+      name = "gperf2json";
+      src = self;
+      nativeBuildInputs = [ meson ninja pkg-config ];
       buildInputs = [ nlohmann_json ];
+      mesonFlags = [
+        "-Dgperf=${runCommand "gperf" {} ''
+          mkdir -p $out
+          tar -x --strip-components 1 -f ${gperf.src} -C $out
+        ''}"
+      ];
     };
   };
 }
