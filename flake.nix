69805046c064bf9897e83ed1463fa218408e9cc4
Nick Cao <nickcao@nichi.co>
2022-09-04 17:26:13 +0800

    init flake

---
+++ flake.nix
@@ -0,0 +1,6 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;{
+    devShells.x86_64-linux.default = mkShell { };
+  };
+}
