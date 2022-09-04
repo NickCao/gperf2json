4eedec8c461b2f092a8a786b277a5280998a92a2
Nick Cao <nickcao@nichi.co>
2022-09-04 19:36:03 +0800

    prototyping

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;{
     devShells.x86_64-linux.default = mkShell {
-      nativeBuildInputs = [ meson ninja gperf ];
+      nativeBuildInputs = [ meson ninja clang-tools ];
     };
   };
 }
--- gperf2json.cc
+++ gperf2json.cc
@@ -1,2 +1,25 @@
+#include "input.h"
+#include <iostream>
+
+class KeywordExt_Factory : public Keyword_Factory {
+  virtual Keyword *create_keyword(const char *allchars, int allchars_length,
+                                  const char *rest);
+};
+
+Keyword *KeywordExt_Factory::create_keyword(const char *allchars,
+                                            int allchars_length,
+                                            const char *rest) {
+  return new KeywordExt(allchars, allchars_length, rest);
+}
+
 int main() {
+  KeywordExt_Factory factory;
+  Input inputter(stdin, &factory);
+  inputter.read_input();
+  KeywordExt_List *list = static_cast<KeywordExt_List *>(inputter._head);
+  for (KeywordExt_List *temp = list; temp; temp = temp->rest()) {
+    auto head = temp->first();
+    fwrite(head->_allchars, sizeof(char), head->_allchars_length, stdout);
+    std::cout << head->_rest << std::endl;
+  };
 }
