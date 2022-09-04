ab30c355f324285ef99c0d278d2b24062ad1cc45
Nick Cao <nickcao@nichi.co>
2022-09-04 20:24:17 +0800

    dump json

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;{
     devShells.x86_64-linux.default = mkShell {
-      nativeBuildInputs = [ meson ninja clang-tools ];
+      nativeBuildInputs = [ meson ninja pkg-config clang-tools ];
+      buildInputs = [ nlohmann_json ];
     };
   };
 }
--- gperf2json.cc
+++ gperf2json.cc
@@ -1,5 +1,10 @@
 #include "input.h"
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <unordered_map>
+
+using json = nlohmann::json;
 
 class KeywordExt_Factory : public Keyword_Factory {
   virtual Keyword *create_keyword(const char *allchars, int allchars_length,
@@ -17,9 +22,17 @@ int main() {
   Input inputter(stdin, &factory);
   inputter.read_input();
   KeywordExt_List *list = static_cast<KeywordExt_List *>(inputter._head);
+  std::unordered_map<std::string, std::string> directives;
   for (KeywordExt_List *temp = list; temp; temp = temp->rest()) {
     auto head = temp->first();
-    fwrite(head->_allchars, sizeof(char), head->_allchars_length, stdout);
-    std::cout << head->_rest << std::endl;
+    auto rest = std::string(head->_rest);
+    auto begin = std::find_if(rest.begin(), rest.end(), [](unsigned char ch) {
+      return !std::isspace(ch);
+    });
+    rest.erase(rest.begin(), begin);
+    directives.emplace(std::string(head->_allchars, head->_allchars_length),
+                       rest.substr(0, rest.find(",")));
   };
+  json a = directives;
+  std::cout << a.dump() << std::endl;
 }
